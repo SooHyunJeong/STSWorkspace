fd5210ead31bbc39f00fa12465fd37fc315a26f8
SooHyunJeong <shjeong@raizcorp.com>
2018-01-23 19:46:20 +0900

    충돌 해결

--- bootstrap/src/main/webapp/resources/vendor/bootstrap/js/bootstrap.bundle.js
+++ bootstrap/src/main/webapp/resources/vendor/bootstrap/js/bootstrap.bundle.js
@@ -2214,10 +2214,10 @@ function runModifiers(modifiers, data, ends) {
   var modifiersToRun = ends === undefined ? modifiers : modifiers.slice(0, findIndex(modifiers, 'name', ends));
 
   modifiersToRun.forEach(function (modifier) {
-    if (modifier.function) {
+	  if (modifier.function) {
       console.warn('`modifier.function` is deprecated, use `modifier.fn`!');
     }
-    var fn = modifier.function || modifier.fn;
+	  var fn = modifier.function || modifier.fn;
     if (modifier.enabled && isFunction(fn)) {
       // Add properties to offsets to make them a complete clientRect object
       // we do this before each modifier to make sure the previous one doesn't
